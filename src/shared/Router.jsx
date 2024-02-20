3e9677677d8c5aeeb9a11bdb63ec0ee347d75a5f
GNooNJo <jjjchoon@nate.com>
2024-02-20 14:23:40 +0900

    feat: 로그인 상태에 따른 조건부 라우팅 구현

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,15 +1,31 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Header from "components/layout/Header";
 import Home from "pages/Home";
 import Detail from "pages/Detail";
-import Header from "components/layout/Header";
+import Login from "pages/Login";
+import Profile from "pages/Profile";
+import { useState } from "react";
 
 const Router = () => {
+  const [isLoggedIn, setLoginState] = useState(true);
+
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detail/:id" element={<Detail />} />
+        {isLoggedIn ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/detail/:id" element={<Detail />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </>
+        )}
       </Routes>
     </BrowserRouter>
   );
