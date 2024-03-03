5ecc9e71868891482e320f6ea5771ee2d6a2e8a5
GNooNJo <jjjchoon@nate.com>
2024-03-03 18:57:27 +0900

    feat: add token time validation

--- src/api/axiosAPI.js
+++ src/api/axiosAPI.js
@@ -1,5 +1,13 @@
 import axios from "axios";
+import { toast } from "react-toastify";
+import { logout } from "shared/redux/modules/authSlice";
 
+let store;
+import("shared/redux/config/configStore").then((module) => {
+  store = module.default();
+});
+
+// 사용자 정보 DB 저장소
 const userAuthApi = axios.create({
   baseURL: "https://moneyfulpublicpolicy.co.kr",
   headers: {
@@ -7,11 +15,48 @@ const userAuthApi = axios.create({
   },
 });
 
+// 메일 리스트 DB 저장소
 const letterDbApi = axios.create({
   baseURL: "http://localhost:5000",
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+userAuthApi.interceptors.request.use(
+  (request) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      request.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+userAuthApi.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    toast.error(error.response.data.message);
+    if (error.response.data.message === "토큰이 만료되었습니다. 다시 로그인 해주세요.") {
+      return store.dispatch(logout());
+    }
+    return Promise.reject(error);
+  }
+);
+
+letterDbApi.interceptors.request.use(
+  async (request) => {
+    const { data } = await userAuthApi.get("/user");
+    if (data.success) return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export { userAuthApi, letterDbApi };
