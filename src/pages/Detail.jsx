28682b0dab984e930a9df4151fc540029ae64bbb
GNooNJo <jjjchoon@nate.com>
2024-03-03 21:40:53 +0900

    fix: 새로고침 시 아무것도 안뜨는 문제 해결

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,13 +1,26 @@
 import { PageContainer } from "components/styles/GlobalStyle";
 import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ArtistPhotoSection from "components/section/ArtistPhotoSection";
 import MailDetailSection from "components/section/MailDetailSection";
+import { useEffect } from "react";
+import { __getMailList } from "shared/redux/modules/mailListSlice";
 
 const Detail = () => {
-  const mailList = useSelector((state) => state.mailList.letters);
-  const id = useParams().id;
+  const { letters: mailList, isLoading } = useSelector((state) => state.mailList);
+  const { id } = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(__getMailList());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <p>로딩중...</p>;
+  }
+
   const foundMail = mailList.find((mail) => mail.id === id);
+
   return (
     <PageContainer>
       {/* 아티스트 사진 section */}
